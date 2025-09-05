2e5e668d954692c137ef42fb9b7067ba5134f6b4
Arltriaa <pokemonrgby@gmail.com>
2025-09-06 03:55:08 +0900

    Delete public/js/api/config.js

--- public/js/api/config.js
+++
@@ -1,6 +0,0 @@
-// /public/js/api/config.js
-export const SUPABASE = {
-  url: 'https://pnmawpyryntgoyxihzzf.supabase.co',   // 콘솔 Settings → API → Project URL
-  anon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubWF3cHlyeW50Z295eGloenpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwODYyNDQsImV4cCI6MjA3MjY2MjI0NH0.14BVgIQP6T1TuQigoweiQGULDr7rgBomqrhNtOLhXE0' // anon public key
-};
-export const STORAGE_BACKEND = 'supabase'; // 기본 Supabase 사용
