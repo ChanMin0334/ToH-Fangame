ee272f12b09f3f3e18fdbc08ff5133ccb8a6ed04
Arltriaa <pokemonrgby@gmail.com>
2025-09-05 23:30:13 +0900

    Update firebase.js

--- public/js/api/firebase.js
+++ public/js/api/firebase.js
@@ -16,12 +16,12 @@ import {
 
 // >>> 네 Firebase 설정으로 교체 (이미 쓰던 값)
 const firebaseConfig = {
-  apiKey:        "YOUR_API_KEY",
-  authDomain:    "YOUR_PROJECT.firebaseapp.com",
-  projectId:     "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId:         "YOUR_APP_ID"
+  apiKey: "AIzaSyA4ilV6tRpqZrkgXRTKdFP_YjAl3CmfYWo",
+  authDomain: "tale-of-heros---fangame.firebaseapp.com",
+  projectId: "tale-of-heros---fangame",
+  storageBucket: "tale-of-heros---fangame.firebasestorage.app",
+  messagingSenderId: "648588906865",
+  appId: "1:648588906865:web:eb4baf1c0ed9cdbc7ba6d0"
 };
 
 // init
