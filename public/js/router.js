4bf2c779e7ae225b8a85157ee0a17d7aa926e726
Arltriaa <pokemonrgby@gmail.com>
2025-09-10 00:49:59 +0900

    Update router.js

--- public/js/router.js
+++ public/js/router.js
@@ -10,7 +10,7 @@ import { showBattle } from './tabs/battle.js';
 import { showEncounter } from './tabs/encounter.js';
 import showExploreRun from './tabs/explore_run.js';
 import { showExploreBattle } from './tabs/explore_battle.js';
-
+import { showBattleLog } from './tabs/battlelog.js';
 
 
 
@@ -31,6 +31,7 @@ export const routes = {
   '#/explore-battle': showExploreBattle,
   // 수정됨: 키(key)에 '#'를 추가하여 다른 라우트와 형식을 통일
   '#/explore-run': showExploreRun,
+  '#/battlelog': showBattleLog, // 신규 추가
   
 };
 
@@ -53,6 +54,7 @@ export function router(){
   // 수정됨: 불필요한 dynamic import를 제거하고 다른 동적 라우트와 동일한 패턴으로 변경
   if(hash.startsWith('#/explore-run/')) return routes['#/explore-run']();
   if(hash.startsWith('#/explore-battle/')) return routes['#/explore-battle']();
+  if(hash.startsWith('#/battlelog/')) return routes['#/battlelog']();
 
 
   (routes[hash] || routes['#/home'])();
