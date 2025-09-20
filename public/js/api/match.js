9d48e857b54125332fc4f40b85007b7d54b0e802
Arltriaa <pokemonrgby@gmail.com>
2025-09-20 13:41:01 +0900

    Update match.js

--- public/js/api/match.js
+++ public/js/api/match.js
@@ -1,6 +1,6 @@
 // /public/js/api/match.js
 import { func } from './firebase.js';
-import { httpsCallable } from 'firebase/functions';
+import { httpsCallable } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-functions.js';
 
 export async function requestMatch(charId, mode){
   const call = httpsCallable(func, 'requestMatch');
